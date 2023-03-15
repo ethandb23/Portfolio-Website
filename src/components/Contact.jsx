5a9a4d6d42679c412c9a3775b38de9edc68a0332
ethandb23 <119861532+ethandb23@users.noreply.github.com>
2023-03-15 04:55:18 -1000

    CONTACT UP

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,34 +7,32 @@ import { EarthCanvas } from './canvas';
 import { SectionWrapper } from '../hoc';
 import { slideIn } from '../utils/motion';
 
-//
-
-//
-
-//3wKn52gpYljqXiidQ
-
 const Contact = () => {
   const formRef = useRef();
   const [form, setForm] = useState({
-    name: '',
-    email: '',
-    message: '',
+    name: "",
+    email: "",
+    message: "",
   });
-}
+
   const [loading, setLoading] = useState(false);
 
   const handleChange = (e) => {
-    const {name, value } = e.target;
-    
-    setForm({ ...form, [name]: value })
-  }
+    const { target } = e;
+    const { name, value } = target;
+
+    setForm({
+      ...form,
+      [name]: value,
+    });
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
     setLoading(true);
 
-    
-    emailjs.send( 
+    emailjs
+      .send(
       'service_s8hmuup',
       'template_a2ci20e',
       {
@@ -42,26 +40,29 @@ const Contact = () => {
         to_name: 'Ethan',
         from_email: form.email,
         to_email: 'ethan@ethan-berk.com',
-        message: form.message
+        message: form.message,
      },
      '3wKn52gpYljqXiidQ'
     )
-    .then (() => {
-    setLoading(false);
-    alert('Thank you! I will reach out as soon as possible!');
+    .then(
+      () => {
+        setLoading(false);
+        alert("Thank you. I will get back to you as soon as possible.");
 
-    setForm({
-      name: '',
-      email: '',
-      message: '',
-    })
-  }, (error) => {
-      setLoading(false)
+        setForm({
+          name: "",
+          email: "",
+          message: "",
+        });
+      },
+      (error) => {
+        setLoading(false);
+        console.error(error);
 
-      console.log(error);
-      
-      alert('Something went wrong...')
-  })
+        alert("Ahh, something went wrong. Please try again.");
+      }
+    );
+};
 
   return (
     <div className="xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden">
@@ -132,5 +133,6 @@ className="xl:flex-1 xl:h-auto m:h-[550px] h-[350px]"
 </motion.div>
     </div>
   )}
+
 
 export default SectionWrapper(Contact, "contact")
