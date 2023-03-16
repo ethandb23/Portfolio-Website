5b86ff6e1425fe7cf66d026dd1bf9833dd432d4c
ethandb23 <119861532+ethandb23@users.noreply.github.com>
2023-03-15 14:45:07 -1000

    NUDIBRANCH

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -1,25 +1,26 @@
 import { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
-
+import { PointLight } from "three";
 
 import CanvasLoader from "../Loader";
 
 
 const Earth = () => {
 
-  const earth = useGLTF('./planet/scene.gltf')
+  const earth = useGLTF('./slug/scene.gltf')
   return (
     <primitive
     object={earth.scene} 
-    scale={2.75}
-    position-y={0}
-    rotation-y={0}
+    scale={150}
+    position-y={-1}
+    rotation-y={5}
       />
   )
 }
 
 const EarthCanvas = () => {
+  const pointLight = new PointLight(0xffffff, 1);
   return (
     <Canvas
       shadows
@@ -29,22 +30,22 @@ const EarthCanvas = () => {
         fov: 45,
         near: 0.1,
         far: 200,
-        position: [-4, 3, 6]
+        position: [10, 10, 10],
        }}
-      >
-          <Suspense fallback={<CanvasLoader />}>
-            <OrbitControls 
-            autoRotate
-            enableZoom={false}
-            maxPolarAngle={Math.PI / 2}
-            minPolarAngle={Math.PI / 2}
-            />
-            
-            <Earth />
-          </Suspense>
+    >
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls 
+          autoRotate
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+        />
+        <Earth />
+        <primitive object={pointLight}
+        intensity={1} position={[-4, 50, 1]} />
+      </Suspense>
     </Canvas>
   )
-
 }
 
 export default EarthCanvas
